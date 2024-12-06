8589ee48d2d74e711daa7c2c64a6168a0805b0ad
Aouab <aouabsss@gmail.com>
2024-12-06 06:17:06 +0100

    initial commit

---
+++ Models.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        model: './model/human/brain.glb',
+        title: "Le cerveau humain",
+        desc: "Le cerveau humain fonctionne comme un réseau de courants océaniques, connectant différentes régions pour échanger des informations, tout comme les océans permettent le transport des nutriments et de l'énergie. De plus, les marées et la lune peuvent être comparées à l'oreille interne, régulant l'équilibre et les cycles rythmiques qui influencent le fonctionnement du cerveau.",
+        zoom: 0.25
+    },
+    {
+        model: './model/human/intestin.gltf',
+        title: "Le système digestif",
+        desc: "Le système digestif agit comme les écosystèmes marins, où les éléments sont décomposés et recyclés pour nourrir la vie, à l'image des cycles biologiques dans l'océan.",
+        zoom: 0.3
+    },
+    {
+        model: './model/human/poumons.glb',
+        title: "Les poumons",
+        desc: "Les poumons ressemblent à l'océan dans leur rôle de régulation des gaz : ils absorbent l'oxygène et rejettent le dioxyde de carbone, tout comme les océans échangent ces gaz avec l'atmosphère.",
+        zoom: 0.3
+    },
+    {
+        model: './model/human/nerfs.glb',
+        title: "Le système nerveux et les nerfs",
+        desc: "Le système nerveux et les nerfs sont comme les courants marins dans l'océan, transmettant des signaux électriques et chimiques à travers le corps, tout comme les courants transportent l'énergie et la chaleur dans les océans pour maintenir l'équilibre de la planète.",
+        zoom: 0.35
+    },
+    {
+        model: './model/human/eyes.glb',
+        title: "Les yeux",
+        desc: "Les yeux peuvent être comparés à l'océan dans leur structure et leur fonction : l'iris, avec ses motifs et ses nuances, évoque les tourbillons et cyclones marins, tandis que leur capacité à refléter et capter la lumière rappelle la surface des eaux.",
+        zoom: 0.15
+    }
+];
